a89ec98ebcee404147a918168d5bf77696c876f9
Jad Kik <j.kik@gastrofix.com>
2020-04-02 19:11:51 +0200

    First draft of US deaths visualization

---
+++ csse_covid_19_data/csse_covid_19_daily_reports/analyze.py
@@ -0,0 +1,42 @@
+from collections import namedtuple, defaultdict
+import glob
+import csv
+import itertools
+import sys
+
+RowA = namedtuple('RowA', 'filename,province_state,country_region,last_update,confirmed,deaths,recovered')
+RowB = namedtuple('RowB', 'filename,province_state,country_region,last_update,confirmed,deaths,recovered,latitude,longitude')
+RowC = namedtuple('RowC', 'filename,FIPS,Admin2,province_state,country_region,Last_Update,Lat,Long_,confirmed,deaths,recovered,Active,Combined_Key')
+
+row_cls_map = {
+    tuple('\ufeffProvince/State,Country/Region,Last Update,Confirmed,Deaths,Recovered,Latitude,Longitude'.split(',')): RowB,
+    tuple('Province/State,Country/Region,Last Update,Confirmed,Deaths,Recovered,Latitude,Longitude'.split(',')): RowB,
+    tuple('\ufeffProvince/State,Country/Region,Last Update,Confirmed,Deaths,Recovered'.split(',')): RowA,
+    tuple('Province/State,Country/Region,Last Update,Confirmed,Deaths,Recovered'.split(',')): RowA,
+    tuple('\ufeffFIPS,Admin2,Province_State,Country_Region,Last_Update,Lat,Long_,Confirmed,Deaths,Recovered,Active,Combined_Key'.split(',')): RowC,
+    tuple('FIPS,Admin2,Province_State,Country_Region,Last_Update,Lat,Long_,Confirmed,Deaths,Recovered,Active,Combined_Key'.split(',')): RowC,
+}
+
+def readrows():
+    for filename in glob.glob('*.csv'):
+        with open(filename, 'r') as f:
+            reader = csv.reader(f)
+            row_cls = row_cls_map[tuple(next(reader))]
+            for line in reader:
+                yield row_cls(filename, *line)
+
+allrows = sorted(readrows(), key=lambda x: (x.country_region, x.filename))
+# rows = itertools.groupby(allrows, lambda x: x.country_region)
+rows = [row for row in allrows if row.country_region == 'US']
+
+total_deaths_by_day = defaultdict(int)
+for filename, us_rows in itertools.groupby(rows, lambda x: x.filename):
+    date = filename[:-4].split('-')
+    date = '{2}-{0}-{1}'.format(*date)
+    total_deaths_by_day[date] += sum(int(x.deaths) if x.deaths else 0 for x in us_rows)
+
+output = dict(total_deaths_by_day)
+
+writer = csv.writer(sys.stdout)
+for date, deaths in sorted(output.items()):
+    writer.writerow((date, deaths))
---
+++ csse_covid_19_data/csse_covid_19_daily_reports/doplot-non-cumulative.py
@@ -0,0 +1,96 @@
+import csv
+import sys
+
+import itertools 
+
+import matplotlib
+import matplotlib.pyplot as plt
+import numpy as np
+
+from matplotlib.dates import (YEARLY, DateFormatter, WEEKLY, DayLocator, date2num, num2date,
+                              rrulewrapper, RRuleLocator, drange)
+import matplotlib.colors as colors
+import matplotlib.cm as cmx
+
+import datetime
+
+# ax.set(xlabel='time (s)', ylabel='voltage (mV)',
+#       title='About as simple as it gets, folks')
+# ax.grid()
+
+rule = rrulewrapper(WEEKLY, interval=1)
+loc = RRuleLocator(rule)
+formatter = DateFormatter('%y-%m-%d')
+date1 = datetime.date(2020, 1, 22)
+date2 = datetime.date(2020, 4, 1)
+delta = datetime.timedelta(days=1)
+
+dates = drange(date1, date2, delta)
+s = np.random.rand(len(dates))  # make up some random y values
+
+with open(sys.argv[1], 'r') as f:
+    covid_cols, covid_rows = np.loadtxt(f, delimiter=',', usecols=(0, 1), unpack=True, dtype={'names': ('date', 'deaths'), 'formats': ('datetime64[D]', 'f4')})  # np.datetime64, np.float))  # skip_header=1
+
+with open(sys.argv[2], 'r') as f:
+    us_2017_cols, us_2017_rows = np.loadtxt(f, delimiter=';', usecols=(0, 2), unpack=True, dtype={'names': ('cause', 'deaths'), 'formats': ('|U64', 'f4')}, skiprows=2)  # remove header and all causes
+    us_2017_cols, us_2017_rows = us_2017_cols[:-1], us_2017_rows[:-1]  # remove other causes
+#    us_2017_cols, us_2017_rows = us_2017_cols[::-1], us_2017_rows[::-1]  # reverse
+
+my_dpi = 96
+width, height = 1280, 860
+
+fig, ax = plt.subplots(figsize=(width/my_dpi, height/my_dpi), dpi=my_dpi)
+
+# plt.figure(figsize=(width/my_dpi, height/my_dpi), dpi=my_dpi)
+
+plt.gca().xaxis.set_major_formatter(formatter)
+#plt.gca().xaxis.set_major_locator(DayLocator())
+plt.gca().xaxis.set_major_locator(loc)
+
+# plt.plot_date(dates, s)
+plt.plot(dates, covid_rows, label='COVID-19 US Deaths', color='0', lw=2)
+
+#plt.hlines(us_2017_rows, dates[0], dates[-1], linestyles='dotted')
+#for label, y in zip(us_2017_cols, us_2017_rows):
+#    va = 'bottom' if label == 'Septicemia' else 'top'
+#    plt.text(dates[0], y, f'Avg. monthly deaths by {label} in 2017', ha='left', va=va, fontsize=10)
+
+def estimate_deaths(yearly, date):
+    end_date = datetime.date(2020, 12, 31)
+    date_diff = end_date - num2date(date).date()
+    return yearly * (1 - date_diff.days / 364.25)
+
+# us_2017_total_col, us_2017_total_row = us_2017_cols.pop(0), us_2017_rows.pop(0)
+
+# yint = np.arange(0, n_lines*10, 10)
+cm_name = 'tab20c'  # twilight jet bwr
+cm = plt.get_cmap(cm_name)
+
+value_cNorm  = colors.Normalize(vmin=0, vmax=max(us_2017_rows))
+value_scalarMap = cmx.ScalarMappable(norm=value_cNorm, cmap=cm)
+index_cNorm  = colors.Normalize(vmin=0, vmax=len(us_2017_cols))
+index_scalarMap = cmx.ScalarMappable(norm=index_cNorm, cmap=cm)
+
+for i, label, y in zip(itertools.count(), us_2017_cols, us_2017_rows):
+    d_xs = [dates[0], dates[-1] + np.timedelta64(2, 'W').astype('float64')]
+    d_ys = [estimate_deaths(y, x) for x in d_xs]
+    colorVal = index_scalarMap.to_rgba(i)
+    linestyle = 'dashed' if d_ys[-1] < max(covid_rows) else 'solid'
+    marker = None  # 'x' if d_ys[-1] < max(covid_rows) else None
+    plt.plot(d_xs, d_ys, label=f'2017 Avg. Deaths: {label}', color=colorVal, lw=1, linestyle=linestyle, marker=marker)
+
+#ax.xaxis.set_major_locator(loc)
+#ax.xaxis.set_major_formatter(formatter)
+#ax.xaxis.set_tick_params(rotation=30, labelsize=10)
+
+#plt.plot(x,y)
+
+# ax.legend()
+plt.legend(bbox_to_anchor=(0., 1.02, 1., .102), loc='lower left', ncol=3, mode="expand", borderaxespad=0., fontsize='x-small')
+# plt.legend(bbox_to_anchor=(1.05, 1), loc='upper left', borderaxespad=0.)
+
+plt.gcf().autofmt_xdate()
+plt.gca().set_ylim([0, covid_rows[-1] * 1.5])
+
+fig.savefig("non-cumulative.png", dpi=my_dpi * 2)
+plt.show()
