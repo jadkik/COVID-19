bd6057b76ab78b770fd90794939bda7e93675815
Jad Kik <j.kik@gastrofix.com>
2020-06-13 02:57:30 +0200

    Update data

--- csse_covid_19_data/csse_covid_19_daily_reports/analyze.py
+++ csse_covid_19_data/csse_covid_19_daily_reports/analyze.py
@@ -7,6 +7,7 @@ import sys
 RowA = namedtuple('RowA', 'filename,province_state,country_region,last_update,confirmed,deaths,recovered')
 RowB = namedtuple('RowB', 'filename,province_state,country_region,last_update,confirmed,deaths,recovered,latitude,longitude')
 RowC = namedtuple('RowC', 'filename,FIPS,Admin2,province_state,country_region,Last_Update,Lat,Long_,confirmed,deaths,recovered,Active,Combined_Key')
+RowD = namedtuple('RowC', 'filename,FIPS,Admin2,province_state,country_region,Last_Update,Lat,Long_,confirmed,deaths,recovered,Active,Combined_Key,Incidence_Rate,Case_Fatality_Ratio')
 
 row_cls_map = {
     tuple('\ufeffProvince/State,Country/Region,Last Update,Confirmed,Deaths,Recovered,Latitude,Longitude'.split(',')): RowB,
@@ -15,6 +16,7 @@ row_cls_map = {
     tuple('Province/State,Country/Region,Last Update,Confirmed,Deaths,Recovered'.split(',')): RowA,
     tuple('\ufeffFIPS,Admin2,Province_State,Country_Region,Last_Update,Lat,Long_,Confirmed,Deaths,Recovered,Active,Combined_Key'.split(',')): RowC,
     tuple('FIPS,Admin2,Province_State,Country_Region,Last_Update,Lat,Long_,Confirmed,Deaths,Recovered,Active,Combined_Key'.split(',')): RowC,
+    ('FIPS', 'Admin2', 'Province_State', 'Country_Region', 'Last_Update', 'Lat', 'Long_', 'Confirmed', 'Deaths', 'Recovered', 'Active', 'Combined_Key', 'Incidence_Rate', 'Case-Fatality_Ratio'): RowD,
 }
 
 def readrows():
